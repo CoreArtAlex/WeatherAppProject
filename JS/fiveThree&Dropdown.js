e4f3198fa4755435fe625227e49dce3661fab378
Juan Camilo <jccifuentes21@gmail.com>
2022-06-06 23:57:25 -0700

    fetch of API key and API Weather data

--- JS/fiveThree&Dropdown.js
+++ JS/fiveThree&Dropdown.js
@@ -1,8 +1,6 @@
-import fetch from 'node-fetch';
-
-
+// import fetch from 'node-fetch';
 async function fetchData(info){
-
+  
   let allData = await fetch(info)
   let allDataJson = await allData.json();
   
@@ -10,14 +8,25 @@ async function fetchData(info){
 
 }
 
-let lat = '49.2608724'
-let long = '-123.113952'
-let key = '' 
+async function setAPIURL(){
+  let keyResponse = await fetch('APIKey.txt')
+  let keyData = await keyResponse.text()
+  
+  let lat = '49.2608724'
+  let long = '-123.113952'
+  let url = await `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${keyData}&units=metric`
 
-const rawFiveThreeData = await fetchData(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=metric`)
+  return url
+}
 
-let fiveThreeData = {}
+window.addEventListener('DOMContentLoaded', async (event) =>{
+  let url = await setAPIURL();
+  let rawFiveThreeData = await fetchData(url)
+  
+  console.log(rawFiveThreeData.list)
+})
 
+// document.getElementById('button2').setAttribute('href', `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=metric`)
+// let fiveThreeData = {}
 
-
-console.log(rawFiveThreeData.list)
+// console.log(rawFiveThreeData.list)
